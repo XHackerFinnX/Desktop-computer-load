8cb38eae076a2f0d86f40fb4964ff8d42ba74cb7
xhackerfinnx <hfalex120@mail.ru>
2025-01-09 08:22:38 +0300

    файл логики мониторинга

---
+++ monitoring.py
@@ -0,0 +1,17 @@
+import psutil
+import time
+from PyQt5.QtCore import QTime
+
+
+class Monitoring:
+    @staticmethod
+    def get_stats():
+        cpu = psutil.cpu_percent()
+        ram = psutil.virtual_memory().percent
+        disk = psutil.disk_usage("/").percent
+        return cpu, ram, disk
+
+    @staticmethod
+    def format_time(start_time):
+        elapsed_time = QTime(0, 0, 0).addSecs(int(time.time() - start_time))
+        return elapsed_time.toString("hh:mm:ss")
